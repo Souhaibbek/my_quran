d5cd56415f63cb9653ba99e12f843b362d433ee5
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2023-07-10 12:00:34 +0100

    add_surah_details_view

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:my_quran/features/home/presentation/views/surah_details_view.dart';
 
 import '../../Features/Splash/presentation/views/splash_view.dart';
 import '../../features/home/presentation/views/home_view.dart';
@@ -6,6 +7,7 @@ import '../../features/home/presentation/views/home_view.dart';
 abstract class AppRouter {
   static const kSplashView = '/';
   static const kHomeView = '/homeView';
+  static const kSurahDetailsView = '/surahDetailsView';
 
   static final router = GoRouter(
     routes: [
@@ -16,6 +18,10 @@ abstract class AppRouter {
       GoRoute(
         path: kHomeView,
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path: kSurahDetailsView,
+        builder: (context, state) => const SurahDetailsView(),
       ),
     ],
   );
