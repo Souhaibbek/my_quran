491423faee65de59aa2e08f5a328c8a3cdf3c471
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2023-07-11 12:53:18 +0100

    cubit_fetch_surahs_data

---
+++ lib/features/home/presentation/manager/cubit/fetch_surahs_data_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:my_quran/features/home/domain/entities/surah_entity/surah_entity.dart';
+import 'package:my_quran/features/home/domain/use_cases/fetch_all_surah_data_use_case.dart';
+
+part 'fetch_surahs_data_state.dart';
+
+class FetchSurahsDataCubit extends Cubit<FetchSurahsDataState> {
+  FetchSurahsDataCubit(this.fetchAllSurahDataUseCase)
+      : super(FetchSurahsDataInitial());
+  final FetchAllSurahDataUseCase fetchAllSurahDataUseCase;
+  Future<void> fetchSurahData() async {
+    emit(FetchSurahsDataLoading());
+    var result = await fetchAllSurahDataUseCase.call();
+
+    result.fold((failure) {
+      emit(
+        FetchSurahsDataFailure(failure.msg),
+      );
+    }, (surahs) {
+      emit(
+        FetchSurahsDataSuccess(surahs),
+      );
+    });
+  }
+}
---
+++ lib/features/home/presentation/manager/cubit/fetch_surahs_data_state.dart
@@ -0,0 +1,19 @@
+part of 'fetch_surahs_data_cubit.dart';
+
+abstract class FetchSurahsDataState {}
+
+class FetchSurahsDataInitial extends FetchSurahsDataState {}
+
+class FetchSurahsDataLoading extends FetchSurahsDataState {}
+
+class FetchSurahsDataFailure extends FetchSurahsDataState {
+  final String errMsg;
+
+  FetchSurahsDataFailure(this.errMsg);
+}
+
+class FetchSurahsDataSuccess extends FetchSurahsDataState {
+  final List<SurahEntity> surahs;
+
+  FetchSurahsDataSuccess(this.surahs);
+}
