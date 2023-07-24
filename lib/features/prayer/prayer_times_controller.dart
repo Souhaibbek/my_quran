66d37353e8104599ce3c502277137b4fdaaed58d
Souhaibbek <Bekkaysouhaib1996@gmail.com>
2023-07-24 19:04:49 +0100

    fix_prayer_timee

--- lib/features/prayer/prayer_times_controller.dart
+++ lib/features/prayer/prayer_times_controller.dart
@@ -32,22 +32,27 @@ class PrayerTimeController {
       params,
       precision: false,
     );
-
+    prayerTimes.fajr = prayerTimes.fajr?.add(const Duration(hours: 1));
     final fajr = DateFormat.jm().format(prayerTimes.fajr!);
     prayerData.add(fajr);
 
+    prayerTimes.sunrise = prayerTimes.sunrise?.add(const Duration(hours: 1));
     final sunrise = DateFormat.jm().format(prayerTimes.sunrise!);
     prayerData.add(sunrise);
 
+    prayerTimes.dhuhr = prayerTimes.dhuhr?.add(const Duration(hours: 1));
     final dhuhr = DateFormat.jm().format(prayerTimes.dhuhr!);
     prayerData.add(dhuhr);
 
+    prayerTimes.asr = prayerTimes.asr?.add(const Duration(hours: 1));
     final asr = DateFormat.jm().format(prayerTimes.asr!);
     prayerData.add(asr);
 
+    prayerTimes.maghrib = prayerTimes.maghrib?.add(const Duration(hours: 1));
     final maghrib = DateFormat.jm().format(prayerTimes.maghrib!);
     prayerData.add(maghrib);
 
+    prayerTimes.isha = prayerTimes.isha?.add(const Duration(hours: 1));
     final isha = DateFormat.jm().format(prayerTimes.isha!);
     prayerData.add(isha);
 
